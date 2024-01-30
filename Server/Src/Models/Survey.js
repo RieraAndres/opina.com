111161d1b72856e068b55ce0b8608d8e44e27d44
Andres Riera <andresriera9999@gmail.com>
2024-01-30 16:11:41 -0300

    Modelos definidos

--- Server/Src/Models/Survey.js
+++ Server/Src/Models/Survey.js
@@ -1,16 +1,40 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  const Survey = sequelize.define("Survey", {
-    id: {
-      type: DataTypes.INTEGER,
-      unique: true,
-      primaryKey: true,
+  const Survey = sequelize.define(
+    "Survey",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        unique: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      imgUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue:
+          "https://res.cloudinary.com/dayd3q0qx/image/upload/v1706641219/Redactar-una-encuesta_s48hah.png",
+      },
+      status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    {
+      // Configuración para evitar que Sequelize agregue createdAt y updatedAt
+      timestamps: false,
+    }
+  );
   return Survey;
 };
--- Server/Src/Models/User.js
+++ Server/Src/Models/User.js
@@ -1,25 +1,36 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  const User = sequelize.define("User", {
-    id: {
-      type: DataTypes.INTEGER,
-      unique: true,
-      primaryKey: true,
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        unique: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      dni: {
+        type: DataTypes.INTEGER,
+      },
+      whatsapp: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    whatsapp: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      unique: true,
-    },
-  });
+    {
+      // Configuración para evitar que Sequelize agregue createdAt y updatedAt
+      timestamps: false,
+    }
+  );
   return User;
 };
