22a22501669e6e46c8612be5b38e5d9fc1224e99
Andres Riera <andresriera9999@gmail.com>
2024-02-27 13:48:22 -0300

    Footer in Home

--- Client/src/Views/Home/Home.jsx
+++ Client/src/Views/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Cards from "../../Components/Cards/Cards";
+import Footer from "../../Components/Footer/Footer";
 import Header from "../../Components/Header/Header"
 function Home() {
   return (
     <div>
       <Header></Header>
       <Cards></Cards>
+      <Footer></Footer>
     </div>
   );
 }
