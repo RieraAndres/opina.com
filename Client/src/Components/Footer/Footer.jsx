b683bbdde91f342c4221c7eb33aa3e74861cf249
Andres Riera <andresriera9999@gmail.com>
2024-02-29 11:04:56 -0300

    Footer links

--- Client/src/Components/Footer/Footer.jsx
+++ Client/src/Components/Footer/Footer.jsx
@@ -1,12 +1,14 @@
 import styles from "../Footer/Footer.module.css"
+import { NavLink,Link } from 'react-router-dom';
+
 function Footer() {
   return (
     <div className={styles.footerContainer}>
         <div className={styles.nav}>
-            <p>INICIO</p>
-            <p>SOBRE OPINA.COM</p>
-            <p>PRIVACIDAD</p>
-            <p>IMPACTO</p>
+            <NavLink to={"/"} style={{textDecoration:"none"}}><p>INICIO</p></NavLink>
+            <NavLink to={"/about"} style={{textDecoration:"none"}}><p>SOBRE OPINA.COM</p></NavLink>
+            <NavLink to={"/about"} style={{textDecoration:"none"}}><p>PRIVACIDAD</p></NavLink>
+            <NavLink to={"/about"} style={{textDecoration:"none"}}><p>IMPACTO</p></NavLink>
         </div>
         <div className={styles.derechos}>
             <p>© 2024, Opina.com todos los derechos reservados</p>
