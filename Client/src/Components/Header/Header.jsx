44405e4dbb805c5e259b6091fec32e87db234c9d
Andres Riera <andresriera9999@gmail.com>
2024-03-12 13:08:51 -0300

    Navbar a Header

--- Client/src/Components/Header/Header.jsx
+++ Client/src/Components/Header/Header.jsx
@@ -2,8 +2,16 @@ import styles from "../Header/Header.module.css"
 function Header() {
   return (
     <div className={styles.headerContainer}>
-      <p className={styles.title}>UNA PLATAFORMA PARA EL CAMBIO</p>
-      <p className={styles.number}>9.852 personas dejaron su firma</p>
+      <div className={styles.sloganContainer}>
+        <p className={styles.title}>UNA PLATAFORMA PARA EL CAMBIO</p>
+        <p className={styles.number}>9.852 personas dejaron su firma</p>
+      </div>
+      <div className={styles.navBar}>
+        <a href={"/"} style={{textDecoration:"none"}}><p>INICIO</p></a>
+        <a href={"/about"} style={{textDecoration:"none"}}><p>SOBRE OPINA.COM</p></a>
+        <a href={"/about"} style={{textDecoration:"none"}}><p>PRIVACIDAD</p></a>
+        <a href={"/about"} style={{textDecoration:"none"}}><p>IMPACTO</p></a>
+      </div>
     </div>
   );
 }
