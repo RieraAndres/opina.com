e7ad75e7805ddb248aadaaff503da61ac280edd3
Andres Riera <andresriera9999@gmail.com>
2024-03-22 11:41:42 -0300

    Agrego numero de respuestas a getAllSurveys

--- Server/Src/Controllers/CrudSurvey/getAllSurveys.js
+++ Server/Src/Controllers/CrudSurvey/getAllSurveys.js
@@ -1,14 +1,39 @@
-const { Survey } = require("../../db");
+const { Survey, Response } = require("../../db");
+const { Sequelize } = require("sequelize");
 
 async function getAllSurveys() {
   try {
-    const allSurveys = await Survey.findAll();
+    const allSurveys = await Survey.findAll({
+      include: [
+        {
+          model: Response,
+          attributes: [],
+        },
+      ],
+      attributes: {
+        include: [
+          [
+            Sequelize.literal(
+              '(SELECT COUNT(*) FROM "Responses" WHERE "Responses"."SurveyId" = "Survey"."id")'
+            ),
+            "responseCount",
+          ],
+        ],
+      },
+    });
 
     if (!allSurveys) {
       throw new Error("No se encontraron las encuestas o no hay encuestas");
     }
 
-    return allSurveys;
+    return allSurveys.map((survey) => ({
+      id: survey.id,
+      title: survey.title,
+      description: survey.description,
+      imgUrl: survey.imgUrl,
+      status: survey.status,
+      responseCount: survey.dataValues.responseCount, // Agregar el número de respuestas asociadas
+    }));
   } catch (error) {
     throw error;
   }
