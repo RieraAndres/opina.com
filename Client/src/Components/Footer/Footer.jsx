9d379c5743ccb55be0248e4d88a0563bc169817f
Andres Riera <andresriera9999@gmail.com>
2024-05-14 12:41:45 -0300

    Agrego navlink para ingresar como administrador

--- Client/src/Components/Footer/Footer.jsx
+++ Client/src/Components/Footer/Footer.jsx
@@ -8,6 +8,7 @@ function Footer() {
             <a href={"/about"} style={{textDecoration:"none"}}><p>SOBRE OPINA.COM</p></a>
             <a href={"/about"} style={{textDecoration:"none"}}><p>PRIVACIDAD</p></a>
             <a href={"/about"} style={{textDecoration:"none"}}><p>IMPACTO</p></a>
+            <a href={"/adminlogin"} style={{textDecoration:"none"}}><p>ADMIN</p></a>
         </div>
         <div className={styles.derechos}>
             <p>© 2024, Opina.com todos los derechos reservados</p>
