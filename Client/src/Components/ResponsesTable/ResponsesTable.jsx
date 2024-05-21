a8697c6ef7ea1f8b0f06b8cbb840582283201582
Andres Riera <andresriera9999@gmail.com>
2024-05-21 13:51:26 -0300

    Agrego opcion para descargar tabla de respuestas de una sola encuesta

--- Client/src/Components/ResponsesTable/ResponsesTable.jsx
+++ Client/src/Components/ResponsesTable/ResponsesTable.jsx
@@ -1,13 +1,46 @@
 import { NavLink } from "react-router-dom";
-import styles from "./ResponsesTable.module.css"
+import styles from "./ResponsesTable.module.css";
+import { CSVLink } from 'react-csv';
 
-function ResponsesTable({responses}) {
-    return (    
-    <div>
-        <NavLink to="/admin"><button className={styles.button}>VOLVER</button></NavLink>
-        <div className={styles.tableContainer}>
-            <p>ESTAS SON LAS RESPUESTAS DE LA ENCUESTA</p>
-           <table className={styles.table}>
+function ResponsesTable({ responses }) {
+    if (!responses || !responses.users) {
+        // Muestra un mensaje de carga o simplemente retorna null si no hay datos todavía
+        return <></>;
+    }
+
+    const users = responses.users;
+    console.log(users);
+
+    // Definir los encabezados del CSV
+    const headers = [
+        { label: 'Nombre', key: 'name' },
+        { label: 'Apellido', key: 'lastName' },
+        { label: 'DNI', key: 'dni' },
+        { label: 'Teléfono', key: 'whatsapp' },
+    ];
+
+    // Formatear los datos para el CSV
+    const csvData = users.map(user => ({
+        name: user.name,
+        lastName: user.lastName,
+        dni: user.dni,
+        whatsapp: user.whatsapp,
+    }));
+
+    // Configuración del reporte CSV
+    const csvReport = {
+        data: csvData,
+        headers: headers,
+        separator: ';', // Aseguramos el uso de la coma como separador
+        filename: `respuestas${responses.title}.csv`,
+    };
+
+    return (
+        <div>
+            <NavLink to="/admin"><button className={styles.button}>VOLVER</button></NavLink>
+            <div className={styles.tableContainer}>
+                <p style={{ textTransform: "uppercase" }}>{responses.title}</p>
+                <table className={styles.table}>
                     <thead>
                         <tr>
                             <th className={styles.celda}>Nombre</th>
@@ -17,19 +50,22 @@ function ResponsesTable({responses}) {
                         </tr>
                     </thead>
                     <tbody>
-                        {responses.map(response => (
-                            <tr>
-                                <td className={styles.celda}>{response.name}</td>
-                                <td className={styles.celda}>{response.lastName}</td>
-                                <td className={styles.celda}>{response.dni}</td>
-                                <td className={styles.celda}>{response.whatsapp}</td>
+                        {users.map(user => (
+                            <tr key={user.dni}>
+                                <td className={styles.celda}>{user.name}</td>
+                                <td className={styles.celda}>{user.lastName}</td>
+                                <td className={styles.celda}>{user.dni}</td>
+                                <td className={styles.celda}>{user.whatsapp}</td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
+                <button className={styles.download}>
+                    <CSVLink {...csvReport} style={{ textDecoration: "none", color: "#7c3422" }}>DESCARGAR</CSVLink>
+                </button>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default ResponsesTable;
