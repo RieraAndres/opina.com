b89a143d34da8d767211fb7fe9fea9fb8a5b127e
Andres Riera <andresriera9999@gmail.com>
2024-05-28 15:56:14 -0300

    Responsive de Header

--- Client/src/Components/Header/Header.jsx
+++ Client/src/Components/Header/Header.jsx
@@ -1,9 +1,30 @@
 import styles from "../Header/Header.module.css"
 import { NavLink } from 'react-router-dom';
+import MenuDesp from "../MenuDesp/MenuDesp";
+import React, { useState, useEffect } from 'react';
+
 
 function Header() {
+
+  // Menu desplegable
+  const [anchoPantalla, setAnchoPantalla] = useState(window.innerWidth);
+
+  const actualizarAnchoPantalla = () => {
+    setAnchoPantalla(window.innerWidth);
+  };
+
+  // Agrega un efecto para escuchar cambios en el ancho de la pantalla
+  useEffect(() => {
+    window.addEventListener('resize', actualizarAnchoPantalla);
+    // Limpia el efecto cuando el componente se desmonta
+    return () => {
+      window.removeEventListener('resize', actualizarAnchoPantalla);
+    };
+  }, []);
   return (
-    <div className={styles.headerContainer}>
+    <div>
+       {anchoPantalla > 500 ? (
+      <div className={styles.headerContainer}>
       <div className={styles.sloganContainer}>
         <p className={styles.title}>UNA PLATAFORMA PARA EL CAMBIO</p>
         <p className={styles.number}>9.852 personas dejaron su firma</p>
@@ -14,6 +35,16 @@ function Header() {
         <NavLink to="/about" style={{textDecoration:"none"}}><p>PRIVACIDAD</p></NavLink>
         <NavLink to="/about" style={{textDecoration:"none"}}><p>IMPACTO</p></NavLink>
       </div>
+    </div>
+    ):(
+      <div className={styles.headerContainer}>
+      <div className={styles.sloganContainer}>
+        <p className={styles.title}>UNA PLATAFORMA PARA EL CAMBIO</p>
+        <p className={styles.number}>9.852 personas dejaron su firma</p>
+        <MenuDesp></MenuDesp>
+      </div>
+      </div>
+    )}
     </div>
   );
 }
