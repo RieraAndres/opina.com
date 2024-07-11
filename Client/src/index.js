49a71cf14bab56a97cd95c94ab8cb0dd5027f973
Andres Riera <andresriera9999@gmail.com>
2024-07-10 23:21:56 -0300

    Deploy

--- Client/src/index.js
+++ Client/src/index.js
@@ -12,7 +12,7 @@ import axios from "axios";
 
 //  axios.defaults.baseURL = "http://localhost:3001";
 
-axios.defaults.baseURL = "opinacom-production.up.railway.app";
+axios.defaults.baseURL = "https://opinacom-production.up.railway.app/";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
